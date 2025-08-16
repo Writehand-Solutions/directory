39b0de62144203cde5c1cfe58344054fc66067f0
Writehand-AI <147965073+Writehand-AI@users.noreply.github.com>
2025-08-15 17:24:57 -0700

    Update directory-card-grid.tsx

--- components/directory-card-grid.tsx
+++ components/directory-card-grid.tsx
@@ -140,7 +140,7 @@ export const EmptyFeaturedGrid = () => {
       punchline: "Next.j, Supabase & Tailwind Starters",
       product_website: "https://www.skool.com/productised-8535/about",
       description:
-        "Join our free community for modern...",
+        "Join our free community for modern experts...",
       logo_src: "/Comm2.png",
       tags: ["featured"],
       labels: ["Community"],
